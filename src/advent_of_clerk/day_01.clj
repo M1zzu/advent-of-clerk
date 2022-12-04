f29c469da3a1bf0b09b9001a2fb63d8d7ceb221d
Moritz Reinhard <m.reinhard@protonmail.ch>
2022-12-04 16:14:51 +0100

    do day 1

--- src/advent_of_clerk/day_01.clj
+++ src/advent_of_clerk/day_01.clj
@@ -1,3 +1,20 @@
 ;; # 🎄 Advent of Clerk: Day 1
 (ns advent-of-clerk.day-01
-  (:require [nextjournal.clerk :as clerk]))
+  (:require [nextjournal.clerk :as clerk]
+            [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+;; Hello Clerk.
+
+^::clerk/no-cache
+(def input (slurp (io/resource "day1.txt")))
+
+(def calories (->> (str/split-lines input)
+                   (mapv parse-long)
+                   (partition-by nil?)
+                   (remove #(= '(nil) %))
+                   (map #(reduce + %))))
+
+(apply max calories)
+
+(reduce + (take 3 (reverse (sort calories))))
