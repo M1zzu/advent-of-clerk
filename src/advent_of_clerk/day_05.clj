e089aa3a36b4e93426b85b7ff7af7cae4a813102
Moritz Reinhard <m.reinhard@protonmail.ch>
2022-12-07 23:35:38 +0100

    finish day 5 part 1 and 2

--- src/advent_of_clerk/day_05.clj
+++ src/advent_of_clerk/day_05.clj
@@ -5,7 +5,7 @@
             [clojure.string :as str]))
 
 ^::clerk/no-cache
-(def input (slurp (io/resource "day-05-sample.txt")))
+(def input (slurp (io/resource "day-05.txt")))
 
 (let [input-partitioned (partition-by str/blank? (str/split-lines input))]
   (def stacks-str (reverse (nth input-partitioned 0)))
@@ -56,20 +56,44 @@ instructions-str
                       (if (every? #(Character/isDigit %) str)
                         (parse-long str)
                         (keyword str)))
-                    val)))
+                    val)
+               (apply hash-map val)))
        instructions-str))
 
 (defn move-crate [crates from to]
   (let [crate (peek (crates from))]
     (-> crates
-      (assoc from (pop (crates from)))
-      (assoc to (conj (crates to) crate)))))
+        (assoc from (pop (crates from)))
+        (assoc to (conj (crates to) crate)))))
 
 (defn move-crate-ntimes [crates instr]
-  (nth (iterate 
-         (fn [x] 
-           (move-crate x (:from instr) (:to instr))) 
-         crates) 
+  (nth (iterate (fn [x]
+                  (move-crate x (:from instr) (:to instr)))
+                crates)
        (:move instr)))
 
-(move-crate-ntimes crates { :move 3 :from 2 :to 1})
+(move-crate-ntimes crates {:move 3 :from 2 :to 1})
+
+;; do all moves
+(defn do-all-moves [] (reduce move-crate-ntimes crates instructions))
+
+(defn get-top-crates [] 
+  (apply str 
+         (map peek (vals (into (sorted-map) (do-all-moves))))))
+
+;; ## Part two
+
+(defn move-n-crates-at-once [crates instr]
+  (let [crates-to-move (take-last (:move instr) (crates (:from instr)))
+        from-stack (crates (:from instr))
+        from-stack-without-crates (into [] (drop-last (:move instr) from-stack))
+        to-stack (crates (:to instr))
+        to-stack-with-crates (apply conj to-stack crates-to-move)]
+    (-> crates
+        (assoc (:to instr)  to-stack-with-crates)
+        (assoc (:from instr) from-stack-without-crates))))
+
+(defn do-all-moves2 [] 
+  (reduce move-n-crates-at-once crates instructions))
+
+(defn get-top-crates2 [] (apply str (map peek (vals (into (sorted-map) (do-all-moves2))))))
